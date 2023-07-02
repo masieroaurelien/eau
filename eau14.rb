bff65441196708ffb908f41a6351d18da5985ad3
Aurelien <masiero.aurelien@outlook.fr>
2023-07-02 23:43:02 +0200

    Eau14

--- eau14.rb
+++ eau14.rb
@@ -1,1 +1,17 @@
 # Créez un programme qui trie les éléments donnés en argument par ordre ASCII.
+
+if ARGV.length < 1
+  puts "error"
+  exit
+end
+
+elements = ARGV.select { |arg| !arg.match?(/\A-?\d+\z/) }
+
+if ARGV.length != elements.length
+  puts "Error"
+  exit
+end
+
+resultat = elements.sort
+
+puts resultat.join(' ')
