d1d5d0aeec013ba71f04db1d301bd234988353d0
Aurelien <masiero.aurelien@outlook.fr>
2023-07-02 15:51:33 +0200

    Eau01

--- eau01.rb
+++ eau01.rb
@@ -1,4 +1,5 @@
-#Créez un programme qui affiche toutes les différentes combinaisons possibles de 2 chiffres dans l’ordre croissant, dans l’ordre croissant. La répétition est volontaire.
+#Créez un programme qui affiche toutes les différentes combinaisons de deux nombre entre 00 et 99 dans l’ordre croissant.
+
 
 (0..99).to_a.combination(2).each do |combination|
   puts "%02d %02d" % combination
