291620a413eecfbcd408658d31aec4843fb5de41
Aurelien <masiero.aurelien@outlook.fr>
2023-07-02 22:58:26 +0200

    Eau10

--- eau10.rb
+++ eau10.rb
@@ -1,1 +1,17 @@
 # Créez un programme qui affiche le premier index d’un élément recherché dans un tableau. Le tableau est constitué de tous les arguments sauf le dernier. L’élément recherché est le dernier argument. Afficher -1 si l’élément n’est pas trouvé.
+
+if ARGV.length < 2
+  puts "-1"
+  exit
+end
+
+tableau = ARGV[0...-1]
+element_recherche = ARGV[-1]
+
+index = tableau.index(element_recherche)
+
+if index.nil?
+  puts -1
+else
+  puts index
+end
