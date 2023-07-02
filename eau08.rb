c3006a324ed30dd8033030820ccd3df6f2d4e941
Aurelien <masiero.aurelien@outlook.fr>
2023-07-02 22:18:02 +0200

    Eau08

--- eau08.rb
+++ eau08.rb
@@ -1,1 +1,21 @@
-# Créez un programme qui détermine si une chaîne de caractères ne contient que des chiffres. 
+# Créez un programme qui détermine si une chaîne de caractères ne contient que des chiffres.
+
+def que_des_chiffres(chaine)
+  result = ""
+
+  chaine.each_char do |caractere|
+    if ('0'..'9').include?(caractere)
+      return "true"
+    else
+      return "false"
+    end
+  end
+
+  return result
+end
+
+chaine = ARGV[0]
+
+resultat = que_des_chiffres(chaine)
+
+puts resultat
