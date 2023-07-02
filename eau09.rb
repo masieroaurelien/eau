86f6ce2174e8484ac609bf0067d9e4c175e2e2fa
Aurelien <masiero.aurelien@outlook.fr>
2023-07-02 22:44:05 +0200

    Eau09

--- eau09.rb
+++ eau09.rb
@@ -1,1 +1,18 @@
 # Créez un programme qui affiche toutes les valeurs comprises entre deux nombres dans l’ordre croissant. Min inclus, max exclus.
+
+cli = ARGV.sort
+
+min = cli[0].to_i
+max = cli[1].to_i
+
+result = []
+
+count = min
+
+while count < max
+ result.push(count)
+ count += 1
+end
+
+
+puts result.join(' ')
